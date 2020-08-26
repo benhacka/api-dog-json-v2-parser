afaeb5c58803f3e9bd488967d8f5ab69505896d6
benhacka <54145505+benhacka@users.noreply.github.com>
2020-08-27 02:26:19 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ setup(name='api-dog-json-v2-parser',
       packages=find_packages(),
       author='rotten_meat',
       install_requires=load_requirements(),
-      url='https://github.com/benhacka/api-dog-json-v2-parser',
+      url='https://github.com/benhacka/api-dog-json-v2-parser/',
       download_url=
-      'https://github.com/benhacka/api-dog-json-v2-parser/archive/v1.0.tar.gz',
+      'https://github.com/benhacka/api-dog-json-v2-parser/archive/v1.1.tar.gz',
       entry_points={
           'console_scripts': [
               'api-dog-pv2=api_dog_parser_v2.parser:main',
