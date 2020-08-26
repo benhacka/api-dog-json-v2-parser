fa1e6d04a8ba3ba525ecef4eef6eae085272b5cb
@rottenmeat <rotten_meat@qwerty1unknwon.com>
2020-08-27 02:36:19 +0300

    Added long_description

--- setup.py
+++ setup.py
@@ -6,11 +6,16 @@ def load_requirements():
         return f.read().splitlines()
 
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='api-dog-json-v2-parser',
       version='1.0',
       packages=find_packages(),
       author='rotten_meat',
       install_requires=load_requirements(),
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/benhacka/api-dog-json-v2-parser/',
       download_url=
       'https://github.com/benhacka/api-dog-json-v2-parser/archive/v1.1.tar.gz',
